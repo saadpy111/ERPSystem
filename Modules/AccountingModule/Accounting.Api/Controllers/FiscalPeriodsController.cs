9b6ebf1d722dad92d9735ca56085d894756967fc
saadpy11 <saadahmad@gmail.com>
2026-05-10 20:27:24 +0300

    refactor(accounting): split fiscal years and periods into separate controllers

--- Modules/AccountingModule/Accounting.Api/Controllers/FiscalPeriodsController.cs
+++ Modules/AccountingModule/Accounting.Api/Controllers/FiscalPeriodsController.cs
@@ -4,106 +4,31 @@ using Microsoft.AspNetCore.Mvc;
 using SharedKernel.Authorization;
 using SharedKernel.Core.Constants.Permissions;
 using Accounting.Application.Features.Fiscal.Commands.CloseFiscalPeriod;
-using Accounting.Application.Features.Fiscal.Commands.CreateFiscalYear;
 using Accounting.Application.Features.Fiscal.Commands.OpenFiscalPeriod;
-using Accounting.Application.Features.Fiscal.Commands.UpdateFiscalYear;
-using Accounting.Application.Features.Fiscal.Commands.CloseFiscalYear;
-using Accounting.Application.Features.Fiscal.Commands.OpenFiscalYear;
 using Accounting.Application.Features.Fiscal.Commands.CreateFiscalPeriod;
 using Accounting.Application.Features.Fiscal.Commands.UpdateFiscalPeriod;
 using Accounting.Application.Features.Fiscal.Commands.DeleteFiscalPeriod;
 using Accounting.Application.Features.Fiscal.Queries.GetFiscalPeriods;
-using Accounting.Application.Features.Fiscal.Queries.GetFiscalYears;
-using Accounting.Application.Features.Fiscal.Queries.GetFiscalYearById;
 using Accounting.Application.Features.Fiscal.Queries.GetFiscalPeriodById;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Accounting.Api.Controllers
 {
     [ApiController]
-    [Route("api/accounting/fiscal")]
+    [Route("api/accounting/fiscal/periods")]
     [Produces("application/json")]
     [ApiExplorerSettings(GroupName = "Accounting")]
-    public class FiscalController : ControllerBase
+    public class FiscalPeriodsController : ControllerBase
     {
         private readonly IMediator _mediator;
 
-        public FiscalController(IMediator mediator)
+        public FiscalPeriodsController(IMediator mediator)
         {
             _mediator = mediator;
         }
 
-        [HttpGet("years")]
-        [HasPermission(AccountingPermissions.FiscalView)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetYears(CancellationToken cancellationToken)
-        {
-            var query = new GetFiscalYearsQuery();
-            var result = await _mediator.Send(query, cancellationToken);
-
-            if (!result.Success)
-                return BadRequest(new { result.Message });
-
-            return Ok(new { success = true, message = result.Message, data = result.Data });
-        }
-
-        [HttpGet("years/{id:int}")]
-        [HasPermission(AccountingPermissions.FiscalView)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> GetYearById([FromRoute] int id, CancellationToken cancellationToken)
-        {
-            var query = new GetFiscalYearByIdQuery { Id = id };
-            var result = await _mediator.Send(query, cancellationToken);
-
-            if (!result.Success)
-                return BadRequest(new { result.Message });
-
-            return Ok(new { success = true, message = result.Message, data = result.Data });
-        }
-
-        [HttpPost("years")]
-        [HasPermission(AccountingPermissions.FiscalManage)]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> CreateYear([FromBody] CreateFiscalYearCommand command, CancellationToken cancellationToken)
-        {
-            var ressult = await _mediator.Send(command, cancellationToken);
-            return Ok(ressult);
-        }
-
-        [HttpPut("years/{id:int}")]
-        [HasPermission(AccountingPermissions.FiscalManage)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> UpdateYear([FromRoute] int id, [FromBody] UpdateFiscalYearCommand command, CancellationToken cancellationToken)
-        {
-            command.Id = id;
-            var result =  await _mediator.Send(command, cancellationToken);
-            return Ok(result);
-        }
-
-        [HttpPost("years/close")]
-        [HasPermission(AccountingPermissions.FiscalManage)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> CloseYear([FromBody] CloseFiscalYearCommand command, CancellationToken cancellationToken)
-        {
-           var result =  await _mediator.Send(command, cancellationToken);
-            return Ok(result);
-        }
-
-        [HttpPost("years/open")]
-        [HasPermission(AccountingPermissions.FiscalManage)]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> OpenYear([FromBody] OpenFiscalYearCommand command, CancellationToken cancellationToken)
-        {
-            var result = await _mediator.Send(command, cancellationToken);
-            return Ok(result);
-        }
-
-        [HttpGet("periods")]
+        [HttpGet]
         [HasPermission(AccountingPermissions.FiscalView)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetPeriods([FromQuery] int? fiscalYearId, CancellationToken cancellationToken)
@@ -117,7 +42,7 @@ namespace Accounting.Api.Controllers
             return Ok(new { success = true, message = result.Message, data = result.Data });
         }
 
-        [HttpGet("periods/{id:int}")]
+        [HttpGet("{id:int}")]
         [HasPermission(AccountingPermissions.FiscalView)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -132,7 +57,7 @@ namespace Accounting.Api.Controllers
             return Ok(new { success = true, message = result.Message, data = result.Data });
         }
 
-        [HttpPost("periods")]
+        [HttpPost]
         [HasPermission(AccountingPermissions.FiscalManage)]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -142,7 +67,7 @@ namespace Accounting.Api.Controllers
             return Ok(new { id });
         }
 
-        [HttpPut("periods/{id:int}")]
+        [HttpPut("{id:int}")]
         [HasPermission(AccountingPermissions.FiscalManage)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -154,7 +79,7 @@ namespace Accounting.Api.Controllers
             return Ok(result);
         }
 
-        [HttpPost("periods/close")]
+        [HttpPost("close")]
         [HasPermission(AccountingPermissions.FiscalManage)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -168,7 +93,7 @@ namespace Accounting.Api.Controllers
             return Ok(new { success = true, message = result.Message });
         }
 
-        [HttpPost("periods/open")]
+        [HttpPost("open")]
         [HasPermission(AccountingPermissions.FiscalManage)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -182,7 +107,7 @@ namespace Accounting.Api.Controllers
             return Ok(new { success = true, message = result.Message });
         }
 
-        [HttpDelete("periods/{id:int}")]
+        [HttpDelete("{id:int}")]
         [HasPermission(AccountingPermissions.FiscalManage)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
---
+++ Modules/AccountingModule/Accounting.Api/Controllers/FiscalYearsController.cs
@@ -0,0 +1,101 @@
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Authorization;
+using SharedKernel.Core.Constants.Permissions;
+using Accounting.Application.Features.Fiscal.Commands.CreateFiscalYear;
+using Accounting.Application.Features.Fiscal.Commands.UpdateFiscalYear;
+using Accounting.Application.Features.Fiscal.Commands.CloseFiscalYear;
+using Accounting.Application.Features.Fiscal.Commands.OpenFiscalYear;
+using Accounting.Application.Features.Fiscal.Queries.GetFiscalYears;
+using Accounting.Application.Features.Fiscal.Queries.GetFiscalYearById;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Accounting.Api.Controllers
+{
+    [ApiController]
+    [Route("api/accounting/fiscal/years")]
+    [Produces("application/json")]
+    [ApiExplorerSettings(GroupName = "Accounting")]
+    public class FiscalYearsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FiscalYearsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [HasPermission(AccountingPermissions.FiscalView)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetYears(CancellationToken cancellationToken)
+        {
+            var query = new GetFiscalYearsQuery();
+            var result = await _mediator.Send(query, cancellationToken);
+
+            if (!result.Success)
+                return BadRequest(new { result.Message });
+
+            return Ok(new { success = true, message = result.Message, data = result.Data });
+        }
+
+        [HttpGet("{id:int}")]
+        [HasPermission(AccountingPermissions.FiscalView)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetYearById([FromRoute] int id, CancellationToken cancellationToken)
+        {
+            var query = new GetFiscalYearByIdQuery { Id = id };
+            var result = await _mediator.Send(query, cancellationToken);
+
+            if (!result.Success)
+                return BadRequest(new { result.Message });
+
+            return Ok(new { success = true, message = result.Message, data = result.Data });
+        }
+
+        [HttpPost]
+        [HasPermission(AccountingPermissions.FiscalManage)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CreateYear([FromBody] CreateFiscalYearCommand command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpPut("{id:int}")]
+        [HasPermission(AccountingPermissions.FiscalManage)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateYear([FromRoute] int id, [FromBody] UpdateFiscalYearCommand command, CancellationToken cancellationToken)
+        {
+            command.Id = id;
+            var result = await _mediator.Send(command, cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpPost("close")]
+        [HasPermission(AccountingPermissions.FiscalManage)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> CloseYear([FromBody] CloseFiscalYearCommand command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return Ok(result);
+        }
+
+        [HttpPost("open")]
+        [HasPermission(AccountingPermissions.FiscalManage)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> OpenYear([FromBody] OpenFiscalYearCommand command, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return Ok(result);
+        }
+    }
+}
